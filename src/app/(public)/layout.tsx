bc380988deada475bf0e41bd3f96818e52c8328e
OvanessTea <OvanessTea@yandex.com>
2025-09-30 10:26:40 +0200

    Add header and public layout

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,9 @@
+import type { PropsWithChildren } from 'react'
+import { Header } from "@/components/Header";
+
+export default function Layout({ children }: PropsWithChildren<unknown>) {
+    return <div>
+        <Header/>
+        {children}
+    </div>
+}
