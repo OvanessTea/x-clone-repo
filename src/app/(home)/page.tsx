132d4114fb4b3a7c014a8ec18ec0020487349ea7
OvanessTea <OvanessTea@yandex.com>
2025-09-30 09:54:06 +0200

    add mocked data to main page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,17 @@
+import { TWEETS } from "@/shared/data/tweets.data";
 import { Tweet } from "./Tweet";
+import type { ITweet } from "@/shared/types/tweet.interface";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex justify-center px-4 py-8">
       <div className="w-full max-w-xl">
         <h1 className="text-3xl font-bold mb-6">Home</h1>
-        <Tweet tweet={{
-          author: "Me",
-          text: "Random text"
-        }} />
+        <div className="space-y-6">
+          {TWEETS.map((tweet: ITweet) => 
+            <Tweet key={`${tweet.author}.${tweet.text}`} tweet={tweet} />
+          )}
+        </div>
       </div>
     </div>
   );
