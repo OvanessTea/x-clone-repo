b991ed6619c854c9e81bc280a20a1807d33e60f1
OvanessTea <OvanessTea@yandex.com>
2025-10-05 04:05:24 +0200

    Add profile page

--- src/app/(public)/profile/page.tsx
+++ src/app/(public)/profile/page.tsx
@@ -1,3 +1,14 @@
+'use client'
+import { PAGES } from "@/config/pages.config";
+import { useRouter } from "next/navigation"
+
 export default function ProfilePage() {
-    return <div>Profile Page</div>
+    const router = useRouter();
+    
+    return (
+        <div>
+            <h1 className="text-3xl fonmt-bold mb-6">Profile</h1>
+            <button className="hover:cursor-pointer" onClick={() => router.push(PAGES.HOME)}>Go to home</button>
+        </div>
+    )
 }
